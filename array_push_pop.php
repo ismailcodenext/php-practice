9c0073cf1eeed61c8b35d2dbc81180e463b3675f
codenextitD <engr.ismail348@gmail.com>
2022-06-11 23:44:10 +0600

    array pop,push,shift,unshift practice

---
+++ array_push_pop.php
@@ -0,0 +1,18 @@
+<?php
+
+$fruits = [ "Mango", "Banana", "Apple"];
+
+// array_pop($fruits);
+// array_push($fruits, "Lemon");
+// array_shift($fruits);
+array_shift($fruits, "Lemon");
+
+
+
+
+echo "<pre>";
+print_r($fruits);
+echo "</pre>";
+
+
+?>
